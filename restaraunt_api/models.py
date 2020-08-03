da4515b899b1d4790474fe1d351d01fe8c8eb653
Damjan Dimitrioski <damjan@thedude.tech>
2020-08-03 15:38:54 +0200

    Initial models
    - typo fix in official django .gitignore file

--- restaraunt_api/models.py
+++ restaraunt_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Menu(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+
+class Product(models.Model):
+    sku = models.CharField(primary_key=True, max_length=50)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    menu = models.ManyToManyField(Menu)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
