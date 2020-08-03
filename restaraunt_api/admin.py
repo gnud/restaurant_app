0134877e609ecf727ca6afe3417d1e3708946978
Damjan Dimitrioski <damjan@thedude.tech>
2020-08-03 20:07:58 +0200

    Registered an admin for Menu and for it's relation - company

--- restaraunt_api/admin.py
+++ restaraunt_api/admin.py
@@ -1,1 +1,18 @@
-# from django.contrib import admin
+from django.contrib import admin
+
+from restaraunt_api.models import Menu, Company
+
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['get_company_name']
+    readonly_fields = []
+
+    @staticmethod
+    def get_company_name(obj):
+        return obj.company.name or 'n/a'
